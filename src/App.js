26e6e4f58f7f05e2576d56e7452bbba87279428c
Paul Thomas <paulthomas2107@gmail.com>
2021-01-25 16:03:11 +0000

    Initial...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  // Vars and const etc
+  let [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  let message = "";
+
+  if (name === "admin" && password === "admin") {
+    message = "Hello Admin !";
+  } else {
+    message = "Who are you ?";
+  }
+  // main return
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <button onClick={buttonWasClicked}>Button</button>
+      <h1>{counter}</h1>
+
+      <div>
+        <span>{message}</span>
+        <input type="text" value={name} onChange={updateTextField} />
+        <input
+          type="password"
+          value={password}
+          onChange={updatePasswordField}
+        />
+        <button onClick={submitForm}>Submit</button>
+      </div>
+      
     </div>
   );
+
+  // functions below
+  function submitForm() {
+    console.log(name, password);
+  }
+
+  function buttonWasClicked() {
+    setCounter((counter += 1));
+  }
+
+  function updateTextField(event) {
+    setName(event.target.value);
+  }
+
+  function updatePasswordField(event) {
+    setPassword(event.target.value);
+  }
 }
 
 export default App;
